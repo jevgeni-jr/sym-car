2c5c61d1847427630cf3d1978403c873ead942cc
Jevgeni Rogotski <jevgeni.rogotski@gmail.com>
2025-03-28 17:14:22 +0200

    Symfony + React

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './js/components/App';
+
+console.log('Running!');
+
+const root = document.getElementById('root');
+if (root) {
+    createRoot(root).render(<App />);
+    console.log('React is running!');
+}
